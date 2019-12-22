32c044ded837a1c431dab4e82b445a7e81955162
rfronteddu <rfronteddu@ihmc.us>
2019-12-22 14:26:44 -0600

    started implementing space poinmt

---
+++ src/main/java/rfronteddu/java_experiments/problems/lpd/LinePatternsDetector.java
@@ -0,0 +1,16 @@
+/*
+ * @author  Roberto Fronteddu
+ * @version 1.0
+ * @since   2019-12-22
+ *
+ * Given a set of n distinct points in the plane, find every (maximal) line segment that
+ * connects a subset of 4 or more of the points.
+ *
+ */
+
+
+package rfronteddu.java_experiments.problems.lpd;
+public class LinePatternsDetector
+{
+
+}
